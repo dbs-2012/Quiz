2a758554250db6ddcd969ed83f2f0c7be4e51014
Darsh Shah <102041543+dbs-2012@users.noreply.github.com>
2022-11-20 17:41:27 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        num: 1,
+        question: "Which sea creature has three hearts?",
+        answer:"3. Octopus",
+        options:[
+            "1. Jellyfish",
+            "2. Sea Horse",
+            "3. Octopus",
+            "4. Lobster"
+        ]
+    }, 
+    {
+        num: 2,
+        question: "CSS stands for?",
+        answer: "1. Cascading Style Sheets",
+        options:[
+            "1. Cascading Style Sheets",
+            "2. Cascading Styling Sheets",
+            "3. Comprehensive Style Sheets",
+            "4. Common Styling Sheets"
+        ]
+    }, 
+    {
+        num: 3,
+        question: "Which is the largest ocean in the world?",
+        answer: "2. The Pacific Ocean",
+        options: [
+            "1. The Indian Ocean",
+            "2. The Pacific Ocean",
+            "3. The Atlantic Ocean",
+            "4. The Arctic Ocean"
+        ]
+    },
+    {
+        num: 4,
+        question: "What does SQL stand for?",
+        answer: "4. Structured Query Language",
+        options:[
+            "1. Styling Question Language",
+            "2. Stylesheet Query Language",
+            "3. Statement Question Language",
+            "4. Structured Query Language"
+        ]
+    },
+    {
+        num: 5,
+        question: "What does PHP stand for?",
+        answer: "2. Hypertext Preprocessor",
+        options:[
+            "1. Hypertext Programming",
+            "2. Hypertext Preprocessor",
+            "3. Hypertext Preprogramming",
+            "4. Hometext Preprocessor"
+        ]
+    }
+];
